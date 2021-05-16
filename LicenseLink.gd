08d4bc9c58f1f15eaa25a072f44d931e5930df60
quwetz <davidu144@gmail.com>
2021-05-16 15:33:48 +0200

    enabled link to license

--- LicenseLink.gd
+++ LicenseLink.gd
@@ -1,5 +1,5 @@
 extends LinkButton
 
 func _on_LicenseLink_pressed():
-#	OS.shell_open("https://github.com/quwetz/bezier/blob/main/license.txt")
-	pass
+	OS.shell_open("https://github.com/quwetz/bezier/blob/main/license.txt")
+
